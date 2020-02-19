07f51c574fd43cdc1411e4196d64275333f5ff39
hbshih <hbshih@gmail.com>
2020-02-19 13:31:18 +0100

    Initial Commit

---
+++ Organ.AI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Organ.AI
+//
+//  Created by Ben on 2020/2/19.
+//  Copyright © 2020 Organ.AI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
